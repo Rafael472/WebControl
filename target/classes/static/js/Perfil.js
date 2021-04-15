8634aeb39363423f0a5a3a73ed08a650800fe22b
Rafael da Silva <rafael._silva@outlook.com.br>
2021-04-15 13:48:47 -0300

    Correção de BUGs
    Corrigindo bug da exclusão de usuários;
    Corrigindo bug do botão alterar situação da permissão;

--- target/classes/static/js/Perfil.js
+++ target/classes/static/js/Perfil.js
@@ -6,6 +6,7 @@ $(function() {
 		var url = botao.attr('href');
 		
 		var acao = botao.data('acao');
+		
 		var response = $.ajax({
 			url: url,
 			type: 'PUT'
@@ -17,18 +18,15 @@ $(function() {
 			var perfilCodigo = botao.data('codigo');
 			if(acao == 'ATIVAR'){
 				$('[data-role=' + perfilCodigo + ']').html('<span class="badge  bg-success text-white">Ativo</span>');
-				botaoInativo = $('#ativar_' + perfilCodigo);
+				botaoInativo = $('#inativar_' + perfilCodigo);
 			}
 			else if(acao == 'INATIVAR') {
 				$('[data-role=' + perfilCodigo + ']').html('<span class="badge  bg-danger text-white">Inativo</span>');
-				botaoInativo = $('#inativar_' + perfilCodigo);
+				botaoInativo = $('#ativar_' + perfilCodigo);
 			}
 			
-			console.log("antes do hide");
-			botao.hide();
-			console.log("antes do show");
-			botaoInativo.show();
-			console.log("depois do show");
+			$("#"+botao[0].id).hide();
+			$("#"+botaoInativo[0].id).show();
 		});
 		
 		response.fail(function(e) {
--- target/classes/static/js/usuarioLista.js
+++ target/classes/static/js/usuarioLista.js
@@ -6,7 +6,7 @@ $('#exclusaoModal').on('show.bs.modal', function(event) {
 	
 	var modal = $(this);
 	var form = modal.find('form');
-	var action = form.attr('action');
+	var action = form.data('url');
 	if (!action.endsWith('/')) {
 		action += '/';
 	}
